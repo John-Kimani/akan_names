719639096e2a3c51d52be137db231a9df5f09d15
John-Kimani <kimwaweru100@gmail.com>
2021-12-12 15:00:24 +0300

    days of the week formula

--- generator.js
+++ generator.js
@@ -2,4 +2,31 @@
 
 function validate () {
     var date = document.getElementById("date").value;
+    var month = document.getElementById("month").value;
+    var year = document.getElementById("year").value;
 }
+
+// initial day of the week formula start
+
+    // Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+
+// initial day of the week formula end
+
+// day of the week formula start sample 1
+
+if (date == " " || month == " " || year == " "){
+    return false;
+}
+else{
+    var cc = year.slice(2);
+    var yy = year.slice(-2);
+    console.log( "cc" + cc + "yy" + yy)
+    let dateOfBirth = year + "-" + month + "-" + date;
+    let dayDate = new date(dateOfBirth);
+    console.log(dayDate);
+    let dayOfTheWeek = dayDate.getDay();
+
+    return dayOfTheWeek;
+}
+
+// day of the week formula end sample 1
