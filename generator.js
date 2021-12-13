db880b715a2869bd0a15a29f4d532639befad9cb
John-Kimani <kimwaweru100@gmail.com>
2021-12-13 19:38:11 +0300

    sets input requirements that validates data

--- generator.js
+++ generator.js
@@ -4,5 +4,12 @@ function validate(){
     var date = document.getElementById("date").value;
     var month = document.getElementById("month").value;
     var year = document.getElementById("year").value;
-    
+
+    if (date == " " || date <=0 || date > 31)
+        { alert("Enter a valid date")}
+        else if (month == " " || month <=0 || month > 12)
+        {alert("Enter a valid month")}
+        else if (year == " " || !year.match(/^\d\d\d\d$/)){
+            alert("Enter a valid year")
+        }
 }
